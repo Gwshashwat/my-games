375f7f526089e90976904dbc764ad2c14071a10c
Gwshashwat <00shashwat.srivastava00@gmail.com>
2025-06-22 13:11:46 +0530

    Update 2. number_guessing.py

--- my games/games/project based/🎮 Game-Based Projects/2. number_guessing.py
+++ my games/games/project based/🎮 Game-Based Projects/2. number_guessing.py
@@ -7,14 +7,17 @@ print('-----------------INSANE NUMBER GUSSING GAME-----------------')
 run = 0
 tries = 0 
 while True:
-    user = input("Do you want to play ? : ").lower()
-    if user == "no":
+    if run == 0:
+        user = input("Do you want to play (Y/N) ? : ").lower()
+    else : 
+        user = input("Do you want to play again (Y/N) ? : ").lower()
+    if user == "n":
         if run == 0:
             print("AT LEAST TRY MY GAME!")
         else:
             print("Thanks for playing.")
         break
-    elif user == "yes":
+    elif user == "y":
         run += 1
         print("EASY = 1\nNORMAL = 2\nHARD = 3\nEXTREME = 4\nINSANE = 5\nNO LIFE = 6")
         d = {1 : "EASY",2 : "NORMAL",3 :"HARD",4 : "EXTREME",5 : "INSANE",6 : "NO LIFE"}
@@ -22,7 +25,7 @@ while True:
             difficulty = int(input("Enter the difficulty : "))
 
             if  0 < difficulty < 7 :
-                number = randint(10**(difficulty),10**(1+difficulty))
+                number = randint(1,10**(1+difficulty))
                 break
             else:
                 print("GOOD PRANK!!")
@@ -31,7 +34,7 @@ while True:
         while True:
             try:
                 tries += 1
-                guess = int(input(f"Enter your guess in between {10**(difficulty)} - {10**(1+difficulty)} 🔢 : "))
+                guess = int(input(f"Enter your guess in between {1} - {10**(1+difficulty)} 🔢 : "))
                 if guess == number :
     
                     print(f"YOU WON !\nNUMBER = {number}\nNUMBER OF GUESSES = {tries}")
@@ -49,4 +52,4 @@ while True:
                 print("BE CAREFULL.")
                 tries -= 1
     else :
-        print("TRY USING YES OR NO.")
+        print("TRY USING Y OR N.")
